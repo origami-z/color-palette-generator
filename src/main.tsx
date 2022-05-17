c89ff0fbdf35958b1a0b5fa95ea912d53e12a24b
origami-z <davidcui2@me.com>
2022-05-17 22:34:26 +0100

    Move uitk css to layer import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { ToolkitProvider } from "@jpmorganchase/uitk-core";
-import "@jpmorganchase/uitk-theme/index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
