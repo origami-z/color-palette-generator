0efbde8be9a4e2c7649f17c920c4f881f233d174
origami-z <davidcui2@me.com>
2023-10-03 22:19:32 +0100

    Adds test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/color-palette-generator/'
-})
+  base: "/color-palette-generator/",
+  // @ts-ignore
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+    },
+  },
+});
